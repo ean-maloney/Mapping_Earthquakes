23bceb99ddcf07d5b91a65bc947fc410d9b6b1bb
Ean Maloney <eanmaloney@gmail.com>
2021-07-14 13:15:56 -0400

    Mapped mult points

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,28 @@
+//Confirm code is running
+console.log("working");
+
+//Create map obj
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+
+//Create tile layer and add to map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(map);
+
+//Get data from cities.js
+let cityData = cities;
+
+cityData.forEach(function(city){
+    console.log(city);
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>") 
+    .addTo(map);
+});
